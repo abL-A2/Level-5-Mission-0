8b067845c1ce47d52c7061c05618811d9fb2f9b6
abL-A2 <amiela@missionreadyhq.com>
2024-11-05 11:38:34 +1300

    feat: NavBar basic dynamic render

---
+++ src/components/NavBar/navBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function NavBar() {
+  // array to contain whatever page names or links need to be in the NavBar
+  const navBarItems = [`Home`, `About`, `Login/Signup`, `Contact`, `Jobs`];
+
+  // function to add a nav button per page or link in the array navBarLinks
+  const navBarDisplay = navBarItems.map((link, index) => {
+    return (
+      <li key={index}>
+        <a>{link}</a>
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <nav>
+        <ul>{navBarDisplay}</ul>
+      </nav>
+    </div>
+  );
+}
