031d88c62ae0219bccff670a519f12f09d8597c3
abL-A2 <amiela@missionreadyhq.com>
2024-11-06 23:48:48 +1300

    feat: drawer menu, final touch-ups

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -5,14 +5,21 @@ import MenuIcon from "@mui/icons-material/Menu";
 // MUI hooks to enable responsive navbar adjustments
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
+// imports for drawer menu that replaces navbar on small screens
+import DrawerMenu from "./navBarMenu";
+import { useState } from "react";
 
 export default function NavBar() {
   // array to contain whatever page names or links need to be in the NavBar
   const navBarItems = [`Home`, `About`, `Login/Signup`, `Contact`, `Jobs`];
 
   const theme = useTheme();
-  //sets up the check for adjusting the nav list for smaller screens
+  // sets up the check for adjusting the nav list for smaller screens
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  // hook for the drawer menu's state
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   // function to add a nav button per page or link in the array navBarLinks
   const navBarDisplay = navBarItems.map((link, index) => {
@@ -36,7 +43,10 @@ export default function NavBar() {
         </div>
         {/* conditional to resize nav elements into small menu icon */}
         {isSmallScreen ? (
-          <MenuIcon sx={{ fontSize: "2rem", cursor: "pointer" }} />
+          <>
+            <MenuIcon sx={{ fontSize: "2rem", cursor: "pointer" }} onClick={toggleMenu} />
+            <DrawerMenu open={menuOpen} onClose={toggleMenu} items={navBarItems} />{" "}
+          </>
         ) : (
           <ul className={styles.navList}>{navBarDisplay}</ul>
         )}
---
+++ src/components/NavBar/navBarMenu.jsx
@@ -0,0 +1,18 @@
+// MUI imports for hamburger drawer menu
+import { List, ListItem, ListItemText, Drawer } from "@mui/material";
+/* eslint-disable react/prop-types */
+
+export default function DrawerMenu({ open, onClose, items }) {
+  return (
+    // drawer menu that replaces navbar links on small screens
+    <Drawer anchor="right" open={open} onClose={onClose}>
+      <List>
+        {items.map((item, index) => (
+          <ListItem button key={index} onClick={onClose}>
+            <ListItemText primary={item} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+}
