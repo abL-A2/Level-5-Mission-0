8af4b897c52258a504d1b6914ceb62fb60ca0558
abL-A2 <amiela@missionreadyhq.com>
2024-11-06 18:02:20 +1300

    feat: media card dynamic render

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,21 +1,26 @@
+// styles
 import styles from "./home.module.css";
+// components
+import Thumbnail from "./thumbnail";
 
-export default function home() {
+export default function Home() {
   return (
     <div>
       <div className={styles.banner}>
         <p>Lorem Ipsum, banner background & search bar goes here</p>
-        <div className={styles.searchContaner}>
+        <div className={styles.searchContainer}>
           <form>
             <input type="text" placeholder="search..." name="search" />
             <button className={styles.submit}>Search</button>
           </form>
         </div>
       </div>
-      <body className={styles.body}>
+      <div className={styles.thumbnailContainer}>
         This container is for the captioned thumbnails
-        <div className={styles.thumbnailGrid}>{/* a function will go here that dynamically renders thumbnails */}</div>
-      </body>
+        <div className={styles.thumbnailGrid}>
+          <Thumbnail />
+        </div>
+      </div>
       <footer className={styles.footer}>Lorem ipsum dolor sit amet</footer>
     </div>
   );
--- src/pages/Home/thumbnail.jsx
+++ src/pages/Home/thumbnail.jsx
@@ -1,37 +1,68 @@
 import styles from "./thumbnail.module.css";
+// MUI imports
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-export default function Thumbnail() {
-  const thumbnailItems = [
-    { image: "/cat-public", caption: `A cat with an orange background`, url: `https://http.cat/` },
-    { image: "/dog-public", caption: `A dog with a bokeh background`, url: `https://http.dog/` },
-    {
-      image: "/piwakawaka-public",
-      caption: `A piwakawaka perched on a branch`,
-      url: `https://en.wikipedia.org/wiki/Fantail`,
-    },
-    {
-      image: "/pukeko-public",
-      caption: `A pukeko striding on turf`,
-      url: `https://www.nzbirdsonline.org.nz/species/pukeko`,
-    },
-  ];
+// example array of thumbnail items to display
+// this should be replaced by
+const mediaCardItems = [
+  {
+    image: "/cat-public.png",
+    caption: `Lorem ipsum dolor cat.`,
+    alt: `A cat with an orange background`,
+    url: `https://http.cat/`,
+  },
+  {
+    image: "/dog-public.jpg",
+    caption: `Lorem ipsum dogor.`,
+    alt: `A dog with a bokeh background`,
+    url: `https://http.dog/`,
+  },
+  {
+    image: "/piwakawaka-public.jpg",
+    caption: `Lorem ipsum dolor piwakawaka`,
+    alt: `A piwakawaka perched on a branch`,
+    url: `https://en.wikipedia.org/wiki/Fantail`,
+  },
+  {
+    image: "/pukeko-public.jpg",
+    caption: `Lorem ipsum dolor pukeko`,
+    alt: `A pukeko striding on turf`,
+    url: `https://www.nzbirdsonline.org.nz/species/pukeko`,
+  },
+];
 
-  const thumbnailDisplay = thumbnailItems.map((thumbnail, index) => {
+export default function MediaCard() {
+  // dynamically renders a thumbnail on the page per thumbnail item
+  const mediaCardDisplay = mediaCardItems.map((card, index) => {
     return (
-      <li className={styles.thumbnail} key={index}>
-        <a href={thumbnail.url}>
-          <figure>
-            <img src={thumbnail.image} />
-            <figcaption>{thumbnail.caption}</figcaption>
-          </figure>
-        </a>
-      </li>
+      // <li className={styles.content} key={index}>
+      //   <a href={thumbnail.url}>
+      //     <figure>
+      //       <img src={thumbnail.image} alt={thumbnail.alt} />
+      //       <figcaption>{thumbnail.caption}</figcaption>
+      //     </figure>
+      //   </a>
+      // </li>
+
+      <Card key={index} sx={{ maxWidth: 345 }}>
+        <CardMedia component="img" image={card.image} alt={card.alt} height="140" sx={{ objectFit: "cover" }} />
+        <CardContent>
+          <Typography gutterBottom variatnt="body2" xs={{ color: "text.secondary" }}>
+            {card.caption}
+          </Typography>
+        </CardContent>
+      </Card>
     );
   });
 
   return (
     <div>
-      <ul>{thumbnailDisplay}</ul>
+      <ul>{mediaCardDisplay}</ul>
     </div>
   );
 }
